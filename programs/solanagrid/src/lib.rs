6ca63965c4c517aa2834762729d6d1721bb1ca5b
sagarrat7 <sagarrat7@gmail.com>
2022-09-24 00:22:52 -0700

    update key to deploy to devnet

--- programs/solanagrid/src/lib.rs
+++ programs/solanagrid/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GPoC1vxA1ZYMDADGdt6qWW6jxtrQphfX4JziaCYdoFkA");
 
 #[program]
 pub mod solanagrid {
