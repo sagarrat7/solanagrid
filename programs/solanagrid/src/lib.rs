502cd951caf0a3aaebe9d5078112a15e3b9561a6
sagarrat7 <sagarrat7@gmail.com>
2022-09-23 23:16:10 -0700

    increment gif counter

--- programs/solanagrid/src/lib.rs
+++ programs/solanagrid/src/lib.rs
@@ -10,6 +10,13 @@ pub mod solanagrid {
       base_account.total_gifs = 0;
       Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+      let base_account = &mut ctx.accounts.base_account;
+      base_account.total_gifs += 1;
+      Ok(())
+    }
+  
   }
   
 
@@ -20,6 +27,12 @@ pub struct StartStuffOff <'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
 }
 
 #[account]
