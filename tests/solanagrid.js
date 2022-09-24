c8c8e950ec9ef55d410d1de532f66aa9e73c85cd
sagarrat7 <sagarrat7@gmail.com>
2022-09-23 23:09:06 -0700

    retrieve account data

--- tests/solanagrid.js
+++ tests/solanagrid.js
@@ -1,12 +1,28 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.Solanagrid;
-  const tx = await program.methods.startStuffOff().rpc();
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  //const tx = await program.methods.startStuffOff().rpc();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
